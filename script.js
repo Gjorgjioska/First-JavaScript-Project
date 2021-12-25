279f4f540ebc5eb42f0f791f325c72d8a14efb83
Gjorgjioska <marina.gorgioska@gmail.com>
2021-12-25 21:16:22 +0100

    Work on implementing the game logic

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const sicretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = sicretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === sicretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > sicretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < sicretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
