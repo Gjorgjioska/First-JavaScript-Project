4a7a01bc8b855aa151b63284aedbc45209374cbd
Gjorgjioska <marina.gorgioska@gmail.com>
2021-12-26 13:01:29 +0100

    Manipulating css styles

--- script.js
+++ script.js
@@ -11,20 +11,57 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const sicretNumber = Math.trunc(Math.random() * 20) + 1;
+let sicretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = sicretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    //When player wins
   } else if (guess === sicretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //when player is too high
   } else if (guess > sicretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //when player is too low
   } else if (guess < sicretNumber) {
-    document.querySelector('.message').textContent = 'Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  sicretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start Guessing...!';
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
